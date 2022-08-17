cc398038a3fff957b9aee9b1ca9395ee5907776a
Abdulrahman Elheyb <45883300+Abdulrahmanelheyb@users.noreply.github.com>
2022-08-17 13:28:36 +0300

    Update contact.tsx
    Added layout to the contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,20 @@
+import {ReactElement} from 'react';
+import AppLayout from '../components/layouts/app';
+
 const Contact = () => {
   return (
     <div>
-
+      contact
     </div>
   );
+}
+
+Contact.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <AppLayout>
+      {page}
+    </AppLayout>
+  )
 }
 
 export default Contact;
